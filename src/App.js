61f33706f03c77b657cd6f7fa40e30eb17976585
Thomas <thomas.lombardi@gmail.com>
2017-04-02 14:55:53 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+import FactsList from './facts-list';
+import SelectedFact from './selected-fact'
+
+class App extends Component {
+
+  factHasBeenSelected(fact) {
+
+  }
+
+
+  render() {
+    return (
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header className="mdl-layout__header">
+          <div className="mdl-layout__header-row">
+            <span className="mdl-layout-title">Title</span>
+            <div className="mdl-layout-spacer"></div>
+            <nav className="mdl-navigation mdl-layout--large-screen-only">
+              <a className="mdl-navigation__link" href="">Link</a>
+            </nav>
+          </div>
+        </header>
+        <div className="mdl-layout__drawer">
+          <span className="mdl-layout-title">Title</span>
+          <nav className="mdl-navigation">
+            <a className="mdl-navigation__link" href="">Search</a>
+            <a className="mdl-navigation__link" href="">Link</a>
+            <a className="mdl-navigation__link" href="">Link</a>
+            <a className="mdl-navigation__link" href="">Link</a>
+          </nav>
+        </div>
+        <main className="mdl-layout__content">
+          <div className="page-content">
+            <FactsList />
+            <SelectedFact />
+          </div>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/selected-fact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SelectedFact extends Component {
+  render() {
+    return (
+      <div className="facts-list">
+        <h2>here's my fact</h2>
+      </div>
+    );
+  }
+}
+
+export default SelectedFact;
